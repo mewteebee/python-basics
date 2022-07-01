1411165ab1a38afcac71fa4d8e04ff2f8bdecb8e
Alex Mutebi <alexandermutebi@gmail.com>
2022-07-01 08:07:01 +0300

    Initial commit

---
+++ python_basics.py
@@ -0,0 +1,15 @@
+# declaring variables 
+name = "martin"
+surname = "odegaard"
+age = 22
+height = 1.78
+preferred_foot = 'L'
+
+# printing variables 
+print("name:", name)
+print("surname:", surname)
+print("age:", age)
+print("height(meters):", height)
+print("preferred foot:", preferred_foot)
+
+
